9f6af854997d36ca78d9d72a74d1ec6ebf85ad3a
Hao Xin <Hao.Xin@jbhunt.com>
2018-10-10 16:26:58 -0500

    firebase info to auth

--- auth/src/app.js
+++ auth/src/app.js
@@ -9,11 +9,11 @@ class App extends Component {
 
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: 'AIzaSyBtxMY4K6uHxv_2e3GD-FWAD2ACX6lPVRE',
-      authDomain: 'authentication-70a18.firebaseapp.com',
-      databaseURL: 'https://authentication-70a18.firebaseio.com',
-      storageBucket: 'authentication-70a18.appspot.com',
-      messagingSenderId: '682333809338'
+      apiKey: 'AIzaSyC2U8-zTNb3bYGi6GNEaMO64G0_rk8MDK0',
+      authDomain: 'auth-958d4.firebaseapp.com',
+      databaseURL: 'https://auth-958d4.firebaseio.com',
+      storageBucket: 'auth-958d4.appspot.com',
+      messagingSenderId: '617230755426'
     });
 
     firebase.auth().onAuthStateChanged((user) => {
